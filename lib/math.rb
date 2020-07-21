301080fbadfd00e7896d94f72239e850f252608a
Seohee  Im <seoheeim@vt.edu>
2020-07-21 15:44:38 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,18 @@
 def addition(num1, num2)
-sum = num1 + num2
-puts sum
+return num1 + num2
 end
 
 def subtraction(num1, num2)
-difference = num2 - num1
-puts difference
+return num2 - num1
 end
 
 def division(num1, num2)
-divide = num1 / num2
-puts divide
+return num1 / num2
 end
 
 def multiplication(num1, num2)
-product = num1 * num2
-puts product
+return num1 * num2
+
 end
 
 def modulo(num1, num2)
