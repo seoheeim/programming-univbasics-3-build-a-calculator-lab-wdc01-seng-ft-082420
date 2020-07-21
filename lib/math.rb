5ca36b14050a31e1e75f33404df727f25b3147a8
Seohee  Im <seoheeim@vt.edu>
2020-07-21 15:43:45 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,25 +1,27 @@
 def addition(num1, num2)
-sum = a + b 
+sum = num1 + num2
 puts sum
-sum 
 end
 
 def subtraction(num1, num2)
-
+difference = num2 - num1
+puts difference
 end
 
 def division(num1, num2)
-
+divide = num1 / num2
+puts divide
 end
 
 def multiplication(num1, num2)
-
+product = num1 * num2
+puts product
 end
 
 def modulo(num1, num2)
-
+return num1 % num2
 end
 
 def square_root(num)
-
+return Math.sqrt(num) 
 end
